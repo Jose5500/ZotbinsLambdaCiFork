95805ef33f66446e3b43c35e456eba7992155381
Jose Lopez <Jglopez3@uci.edu>
2022-05-21 08:01:30 -0700

    added sql statement

--- models/db_models_test.py
+++ models/db_models_test.py
@@ -7,14 +7,12 @@ def adding_new_bin_tests():
     previous_number_of_bins = len(session.query(BinInfo).all())
     new_bin = BinInfo(id=-1, uuid=-1, lat=-1, lon=-1, bin_type="T")
 
-    print(f'previous_number_of_bins = {previous_number_of_bins}')
-
     session.add(new_bin)
 
     new_number_of_bins = len(session.query(BinInfo).all())
-    print(f'new number of bins =  {new_number_of_bins}')
+    assert previous_number_of_bins == new_number_of_bins - 1
 
-    assert previous_number_of_bins == new_number_of_bins - 2
+    print(session.execute("SELECT * FROM BinInfo WHERE id = -1"))
 
 
 
